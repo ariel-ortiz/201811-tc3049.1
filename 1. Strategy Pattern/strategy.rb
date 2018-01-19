0d0b172559a37c1cbc162881a59d93fe0beded68
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-01-19 12:15:33 -0600

    Class Exercise: Strategy Pattern

---
+++ 1. Strategy Pattern/strategy.rb
@@ -0,0 +1,45 @@
+class Student
+
+  attr_reader :id, :name, :gender, :gpa
+
+  def initialize(id, name, gender, gpa)
+    @id = id
+    @name = name
+    @gender = gender
+    @gpa = gpa
+  end
+
+end
+
+class StudentStrategy
+
+  def execute(array)
+    raise 'Abstract method called!'
+  end
+
+end
+
+class Course
+
+  def strategy=(new_strategy)
+    if !new_strategy.is_a? StudentStrategy
+      raise 'Invalid argument. Was expecting a StudentStrategy.'
+    end
+    @strategy = new_strategy
+  end
+
+  def initialize
+    @students = []
+    @strategy = nil
+  end
+
+  def add_student(student)
+    @students.push(student)
+  end
+
+  def execute
+    @strategy.execute(@students)
+  end
+
+end
+
