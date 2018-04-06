aab71e2a43ae4d8d5222ab202031190b13ec0750
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-04-06 22:27:52 +0000

    First examples using Sinatra.

---
+++ 6. Sinatra/throw.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+
+# before we process a route, we'll set the response as
+# plain text and set up an array of viable moves that
+# a player (and the computer) can perform
+before do
+  content_type :txt
+  @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
+  @throws = @defeat.keys
+end
+
+get '/throw/:type' do
+  # the params[] hash stores querystring and form data.
+  player_throw = params[:type].to_sym
+  
+  # in the case of a player providing a throw that is not valid,
+  # we halt with a status code of 403 (Forbidden) and let them
+  # know they need to make a valid throw to play.
+  if !@throws.include?(player_throw)
+    halt 403, "You must throw one of the following: #{@throws}"
+  end
+
+  # now we can select a random throw for the computer
+  computer_throw = @throws.sample
+
+  # compare the player and computer throws to determine a winner
+  if player_throw == computer_throw
+    "You tied with the computer. Try again!"
+  elsif computer_throw == @defeat[player_throw]
+    "Nicely done; #{player_throw} beats #{computer_throw}!"
+  else
+    "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
+  end
+end
